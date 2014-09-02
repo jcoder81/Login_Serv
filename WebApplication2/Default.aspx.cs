b47be8453d9da1a86c4b1505f5886f1789090079
jcoder81 <bfaheem25@gmail.com>
2014-09-02 15:25:54 -0500

    Changes

---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedval = DropDownList1.SelectedItem.Value;
+            Response.Write(selectedval);
+        }
+    }
+}
